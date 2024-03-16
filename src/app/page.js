eb8b9209d2bc1b2aae88b7c7507db376f9b19bb1
dolvin17 <huertas876@gmail.com>
2024-03-16 09:23:32 +0100

    canvas working

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,160 @@
+"use client";
+import cn from "clsx";
+import { useEffect, useState } from "react";
+import { useRef } from "react";
 import Image from "next/image";
+import { Londrina_Solid} from "next/font/google";
 
-export default function Home() {
+const background = ["bg1.svg", "bg2.svg"];
+const body = ["body1.svg", "body2.svg", "body3.svg", "body4.svg" ];
+const glasses = ["glasses1.svg", "glasses2.svg", "glasses3.svg", "glasses4.svg" ];
+const lvl = ["lvl1.svg", "lvl2.svg", "lvl3.svg", "lvl4.svg"];
+
+export default function Mint() {
+  const [selectedBackground, setBackground] = useState("");
+  const [selectedBody, setBody] = useState("");
+  const [selectedGlasses, setGlasses] = useState("");
+  const [selectedLvl, setLvl] = useState("");
+  const [isNFTminting, setNFTminting] = useState(false);
+
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    if (
+      selectedBackground &&
+      selectedBody &&
+      selectedGlasses &&
+      selectedLvl &&
+      ctx
+    ) {
+      const backgroundImage = new globalThis.Image();
+      backgroundImage.onload = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+        const bodyImage = new globalThis.Image();
+        bodyImage.onload = () => {
+          ctx.drawImage(bodyImage, 0, 0, canvas.width, canvas.height);
+        };
+        bodyImage.src = selectedBody;
+     
+        const lvlImage = new globalThis.Image();
+        lvlImage.onload = () => {
+          ctx.drawImage(lvlImage, 0, 0, canvas.width, canvas.height);
+        };
+        lvlImage.src = selectedLvl;
+		const glassesImage = new globalThis.Image();
+        glassesImage.onload = () => {
+          ctx.drawImage(glassesImage, 0, 0, canvas.width, canvas.height);
+        };
+        glassesImage.src = selectedGlasses;
+      };
+      backgroundImage.src = selectedBackground;
+    }
+  }, [selectedBackground, selectedBody, selectedGlasses, selectedLvl]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="flex flex-col items-center pt-24 mx-auto   rounded-lg sm:mx-[200px] sm:px-16 w-fill bg-nouns-pastel">
+        <div className="flex flex-col py-8">
+          <h1 className="font-sans text-4xl text-[#594440] border-4 border-dashed font-bold text-center border-b-[#C0ABA7]">
+            Create your PG ID Avatar
+          </h1>
+    
+		  
+        </div>
+        <div className="grid gap-4 sm:grid-cols-2">
+          <div className="w-full gap-8 p-2 rounded-lg bg-white/50">
+		  <div className="flex flex-col p-1 mx-4 ">
+              <h3 className="w-[100px] font-thin text-[#594440]">Background</h3>
+              <div className="flex flex-row">
+                {background.map((bg) => (
+                  <img
+                    src={bg}
+                    key={bg}
+                    onClick={() => setBackground(bg)}
+                    className={`w-20 h-20 cursor-pointer rounded-lg ${
+                      bg === selectedBackground ? "border-2 border-white" : ""
+                    } mr-4`}
+                  />
+                ))}
+              </div>
+            </div>
+		  <div className="relative flex flex-col p-1 mx-4">
+			
+              <h3 className="w-[100px] font-thin text-[#594440]">Lvl</h3>
+              <div className="flex flex-row">
+                {lvl.map((mt) => (
+                  <img
+                    src={mt}
+                    key={mt}
+                    onClick={() => setLvl(mt)}
+                    className={`w-20 h-20 cursor-pointer rounded-lg ${
+                      mt === selectedLvl ? "border-2 border-white" : ""
+                    } mr-4`}
+                  />
+                ))}
+              </div>
+            </div>
+            
+			<div className="flex flex-col p-1 mx-4">
+              <h3 className="w-[100px] font-thin text-[#594440]">glasses</h3>
+              <div className="flex flex-row">
+                {glasses.map((eye) => (
+                  <img
+                    src={eye}
+                    key={eye}
+                    onClick={() => setGlasses(eye)}
+                    className={`w-20 h-20 cursor-pointer rounded-lg ${
+                      eye === selectedGlasses ? "border-2 border-white" : ""
+                    } mr-4`}
+                  />
+                ))}
+              </div>
+            </div>
+            <div className="flex flex-col p-1 mx-4">
+              <h3 className="w-[100px] font-thin text-[#594440]">Body</h3>
+              <div className="flex flex-row">
+                {body.map((bd) => (
+                  <img
+                    src={bd}
+                    key={bd}
+                    onClick={() => setBody(bd)}
+                    className={`w-20 h-20 cursor-pointer rounded-lg ${
+                      bd === selectedBody ? "border-2 border-white" : ""
+                    } mr-4`}
+                  />
+                ))}
+              </div>
+            </div>
+            <button
+              disabled={isNFTminting}
+              className={cn(
+                "w-full py-2 mt-2 bg-black cursor-pointer rounded-xl p1 border-1",
+                selectedBackground &&
+                  selectedBody &&
+                  selectedGlasses &&
+                  selectedLvl
+                  ? ""
+                  : "opacity-0 pointer-events-none"
+              )}
+            >
+              {isNFTminting ? "Minting..." : "Mint NFT"}
+            </button>
+          </div>
+          <div
+            className={`w-full p-8 rounded-lg bg-white/50 ${
+              selectedBackground &&
+              selectedBody &&
+              selectedGlasses &&
+              selectedLvl &&
+              "bg-white/50"
+            }`}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <canvas ref={canvasRef} width={400} height={400} />
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
